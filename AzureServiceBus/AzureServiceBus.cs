9b5aba7bfbcf57969bfc12fc8de8301512a85e77
faroo <faroo@DESKTOP-44GPCRV>
2025-01-25 11:18:39 +0500

    Add project files.

---
+++ AzureServiceBus/AzureServiceBus.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace Starter
+{
+    internal class AzureServiceBus
+    {
+        private readonly string _connectionString;
+        private readonly ServiceBusClient _serviceBusClient;
+        private readonly string queueName;
+        public AzureServiceBus(string connectionString, string queuename)
+        {
+            _serviceBusClient = new ServiceBusClient(connectionString);
+            _connectionString = connectionString;
+            queueName = queuename;
+        }
+
+        public async Task SendMessageAsync()
+        {
+            // Create a ServiceBusSender object by invoking the CreateSender method on the ServiceBusClient object, and specifying the queue name. 
+            ServiceBusSender sender = _serviceBusClient.CreateSender(queueName);
+
+            // Create a new message to send to the queue.
+            string messageContent = "Order new crankshaft for eBike.";
+            var message = new ServiceBusMessage(messageContent);
+
+            // Send the message to the queue.
+            await sender.SendMessageAsync(message);
+        }
+
+        public async Task ReceiveMessageAsync()
+        {
+            // Create a ServiceBusSender object by invoking the CreateSender method on the ServiceBusClient object, and specifying the queue name. 
+            ServiceBusReceiver receiver = _serviceBusClient.CreateReceiver(queueName);
+
+            // Send the message to the queue.
+            await receiver.ReceiveMessagesAsync(1, TimeSpan.FromSeconds(1));
+        }
+    }
+}
---
+++ AzureServiceBus/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a value");
+
+var name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+    Console.WriteLine("Enter a valid value");
+
+
+    Console.WriteLine($"Message send to: {name}");
