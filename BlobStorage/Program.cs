b530ef30dcd25a0f93726aeff3f76505321c8561
Muhammad Farooq Chaudhry <farooqchaudhry749@outlook.com>
2025-01-26 18:16:09 +0500

    web app added

---
+++ BlobStorage/Program.cs
@@ -0,0 +1,15 @@
+﻿//using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING", EnvironmentVariableTarget.Machine);
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    Console.WriteLine("Please enter a storage account connection string in Machine's environment variable 'AZURE_STORAGE_CONNECTION_STRING'");
+}
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
